60a9a6899c9c3f61247edba79c9bc060bc34ba46
Emmanuel Reyes <ext.emmanuel.reyes@pricetravel.com.mx>
2023-03-16 18:42:27 -0600

    Se agrega clase Startup en cliente

---
+++ ControlDeMetas.Client/Client/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace ControlDeMetas.Client
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configura los servicios aquí
+        }
+
+        public void Configure(WebAssemblyHostBuilder builder)
+        {
+            // Configura la aplicación aquí
+        }
+    }
+}
