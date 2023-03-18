1222c39dad6f020fe1bd89b34e5f1f360f109804
Emmanuel Reyes <ext.emmanuel.reyes@pricetravel.com.mx>
2023-03-18 11:49:51 -0600

    Se agrega proyecto de base de datos

---
+++ ControlDeMetas.Database/dbo/Tables/Meta.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Meta] (
+    [Id]            BIGINT         IDENTITY (1, 1) NOT NULL,
+    [Nombre]        NVARCHAR (50)  NOT NULL,
+    [FechaCreacion] DATETIME       CONSTRAINT [DF_Meta_FechaCreacion] DEFAULT (getdate()) NOT NULL,
+    [TotalTareas]   INT            CONSTRAINT [DF_Meta_TotalTareas] DEFAULT ((0)) NOT NULL,
+    [Cumplimiento]  DECIMAL (5, 2) CONSTRAINT [DF_Meta_Cumplimiento] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_Meta] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ ControlDeMetas.Database/dbo/Tables/Tarea.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Tarea] (
+    [Id]            BIGINT        IDENTITY (1, 1) NOT NULL,
+    [IdTarea]       BIGINT        NOT NULL,
+    [Nombre]        NVARCHAR (50) NOT NULL,
+    [FechaCreacion] DATETIME      CONSTRAINT [DF_Tarea_FechaCreacion] DEFAULT (getdate()) NOT NULL,
+    [Estatus]       INT           CONSTRAINT [DF_Tarea_Estatus] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_Tarea] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Tarea_Meta] FOREIGN KEY ([IdTarea]) REFERENCES [dbo].[Meta] ([Id])
+);
+
